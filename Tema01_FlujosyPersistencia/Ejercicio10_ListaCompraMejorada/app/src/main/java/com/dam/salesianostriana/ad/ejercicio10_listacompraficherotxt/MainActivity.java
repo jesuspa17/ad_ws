ead78965785940015bb2de90cf76670e8739c099
jesuspa17 <jpallares94.jp@gmail.com>
2015-10-29 22:58:58 +0100

    ListaCompraFinal

--- Tema01_FlujosyPersistencia/Ejercicio10_ListaCompraMejorada/app/src/main/java/com/dam/salesianostriana/ad/ejercicio10_listacompraficherotxt/MainActivity.java
+++ Tema01_FlujosyPersistencia/Ejercicio10_ListaCompraMejorada/app/src/main/java/com/dam/salesianostriana/ad/ejercicio10_listacompraficherotxt/MainActivity.java
@@ -3,6 +3,7 @@ package com.dam.salesianostriana.ad.ejercicio10_listacompraficherotxt;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -21,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
 
     EditText cantidad, concepto;
     Button anyadir, resetear;
-
     ListView listaCompra;
 
     DataOutputStream dos = null;
@@ -39,50 +40,96 @@ public class MainActivity extends AppCompatActivity {
         listaCompra = (ListView) findViewById(R.id.listViewProductos);
 
 
+        final ArrayList<String> productos = new ArrayList<String>();
+
+        //Lee siempre el archivo para cargar en el listView la lista que ya esté almacenada.
+        try {
+            //Se abre flujo de lectura, lee según como esté almacenada la información
+            //en el fichero y lo añade a la lista que se mostrará en el listView
+            dis = new DataInputStream(openFileInput("lista_compra.dat"));
+
+            String concep;
+            int canti;
+            char separador;
+
+            while (dis.available()>0){
+                canti = dis.readInt();
+                separador = dis.readChar();
+                concep = dis.readUTF();
+                //Se añaden elementos
+
+                productos.add(canti + " " + concep);
+                Log.i("LEIDO: ", canti + " " + concep);
+            }
+
+            dis.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         anyadir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 try {
+                    //Se obtienen los datos de los EditTexts
                     int cant = Integer.parseInt(String.valueOf(cantidad.getText()));
                     String conc = concepto.getText().toString();
 
-                    ArrayList<String> productos = new ArrayList<String>();
-                    String concep;
-                    int canti;
-                    char separador;
-
-                    dis = new DataInputStream(openFileInput("lista_compra.dat"));
-
-                    while ((canti = dis.readInt()) != -1){
-                        separador = dis.readChar();
-                        concep = dis.readUTF();
-
-                        productos.add(canti +" "+ concep);
-                    }
-
-                    //Se instancia el flujo de escritura.
+                    //Se abre flujo de escritura, se escriben los datos obtenidos de los correspodientes
+                    //EditTexts y se añaden a la lista que mostraremos en el ListView
                     dos = new DataOutputStream(openFileOutput("lista_compra.dat", Context.MODE_APPEND));
-                    //Se almacenan los datos.
                     dos.writeInt(cant);
                     dos.writeChar(';');
                     dos.writeUTF(conc);
-
-
-
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, productos);
-
-                    listaCompra.setAdapter(adapter);
-
-                    listaCompra.deferNotifyDataSetChanged();
-
                     dos.close();
-                    dis.close();
+
+                    productos.add(cant + " " + conc);
+
+                    Log.i("ESCRITO Y AÑADIDO: ", cant + " " + conc);
+
+                    //Vacía los campos
+                    cantidad.setText("");
+                    concepto.setText("");
 
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
+                }
+
+            }
+        });
+
+
+        //Coloca el ArrayList en el listView a través de ArrayAdapter
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, productos);
+
+        listaCompra.setAdapter(adapter);
+
+        //Vacía el fichero y limpia el listView y el ArrayList
+        resetear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(productos.isEmpty()){
+
+                }else {
+                    productos.clear();
+                    adapter.clear();
+                    try {
+                        dos = new DataOutputStream(openFileOutput("lista_compra.dat", Context.MODE_PRIVATE));
+                        dos.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(MainActivity.this, "Lista limpiada correctamente", Toast.LENGTH_SHORT).show();
                 }
 
 
