386a03e5cecfaa7c2e47484b1f972dd13313f843
jesuspa17 <jpallares94.jp@gmail.com>
2015-12-11 13:29:31 +0100

    Proyecto kilo

---
+++ Tema04_Bases_de_datos_Android/Ejercicio01_Kilo/greendaogenerator/src/main/java/com/dam/salesianostriana/ad/greendaogenerator/GreenDaoGenerator.java
@@ -0,0 +1,63 @@
+package com.dam.salesianostriana.ad.greendaogenerator;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
+import de.greenrobot.daogenerator.Schema;
+import de.greenrobot.daogenerator.ToMany;
+
+public class GreenDaoGenerator {
+    public static void main(String[] args) {
+
+        Schema schema = new Schema(1000, "de.greenrobot.daoexample");
+        createTables(schema);
+        try {
+
+
+            if (!Files.isDirectory(Paths.get("./src-gen")))
+                Files.createDirectory(Paths.get("./src-gen"));
+
+            new DaoGenerator().generateAll(schema, "../app/src/main/java/com/dam/salesianostriana/ad/ejercicio01_kilo/grendao");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void createTables(Schema schema){
+
+        //Tabla alimento
+        Entity alimento = schema.addEntity("Alimento");
+        alimento.addIdProperty();
+        alimento.addStringProperty("nombre").notNull();
+
+
+        //Tabla caja
+        Entity caja = schema.addEntity("Caja");
+        caja.addIdProperty();
+        caja.addIntProperty("numero").notNull();
+
+        //Tabla alimCaja
+        Entity alimCaja = schema.addEntity("AlimCaja");
+        alimCaja.addIdProperty();
+        alimCaja.addIntProperty("cantidad").notNull();
+
+        Property alimento_id = alimCaja.addLongProperty("alimento_id").notNull().getProperty();
+        alimCaja.addToOne(alimento, alimento_id);
+
+        ToMany alimetoToAlimCaja = alimento.addToMany(alimento, alimento_id);
+        alimetoToAlimCaja.setName("alimenos");
+
+        Property caja_id = alimCaja.addLongProperty("caja_id").notNull().getProperty();
+        alimCaja.addToOne(caja, caja_id);
+
+        ToMany cajaToAlimCaja = caja.addToMany(caja,caja_id);
+        cajaToAlimCaja.setName("cajas");
+
+
+    }
+
+}
